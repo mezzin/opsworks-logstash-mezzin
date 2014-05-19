422320f975e4e7dee77cc5f3c26625429b2854e8
Justin Kieft <mezzin@mezzin.com>
2014-05-19 14:52:52 +0200

    learning chef

--- custom/metadata.rb
+++ custom/metadata.rb
@@ -5,6 +5,6 @@ license          "All rights reserved"
 description      "Installs/Configures custom"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "0.0.1"
-recipe           "custom::install_sqs_river_plugin", "installing SQS river plugin for elasticsearch"
 
-depends "install_sqs_river_plugin"
+provides 'custom'
+provides 'custom::install_sqs_river_plugin'
