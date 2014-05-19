7245c4211169f2fe4e534e5e888e4c55cdbfbac1
Justin Kieft <mezzin@mezzin.com>
2014-05-19 11:35:37 +0200

    updated custom

---
+++ custom/metadata.rb
@@ -0,0 +1,8 @@
+name             'custom'
+maintainer       'mezzin '
+maintainer_email 'mezzin@mezzin.com'
+license          'All rights reserved'
+description      'Installs/Configures custom'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
