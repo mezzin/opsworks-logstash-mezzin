2676633f49ed1793aacbecbf5a196f21dd24fe08
Justin Kieft <mezzin@mezzin.com>
2014-05-19 13:02:43 +0200

    minor fix

--- custom/recipes/install_elasticsearch_sqs_river.rb
+++ custom/recipes/install_elasticsearch_sqs_river.rb
@@ -5,6 +5,6 @@
 # Copyright (C) 2014 YOUR_NAME
 # 
 #
-package "install_sqs_river_plugin" do
+execute "install_sqs_river_plugin" do
   command "/usr/local/elasticsearch/bin/plugin -i river-amazonsqs -u https://s3-eu-west-1.amazonaws.com/albogdano/river-amazonsqs.zip"
 end
