40aaa26e05b0a96b286e4b8a7d630cf7945fc36b
Justin Kieft <mezzin@mezzin.com>
2014-05-19 14:19:48 +0200

    fixed the recipe issues

--- custom/metadata.rb
+++ custom/metadata.rb
@@ -1,10 +1,10 @@
-name             'custom'
-maintainer       'mezzin '
-maintainer_email 'mezzin@mezzin.com'
-license          'All rights reserved'
-description      'Installs/Configures custom'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
-recipe           'custom::install_sqs_river_plugin'
+name             "custom"
+maintainer       "mezzin "
+maintainer_email "mezzin@mezzin.com"
+license          "All rights reserved"
+description      "Installs/Configures custom"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version          "0.0.1"
+recipe           "custom::install_sqs_river_plugin", "installing SQS river plugin for elasticsearch"
 
 depends "install_sqs_river_plugin"
