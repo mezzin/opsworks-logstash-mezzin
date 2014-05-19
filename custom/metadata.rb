9c871e16d2ce9b12cd522846b64ed6ad94e14110
Justin Kieft <mezzin@mezzin.com>
2014-05-19 16:31:15 +0200

    custom stuff

--- custom/metadata.rb
+++ custom/metadata.rb
@@ -1,10 +1,8 @@
 name             "custom"
 maintainer       "mezzin "
 maintainer_email "mezzin@mezzin.com"
-license          "All rights reserved"
 description      "Installs/Configures custom"
-long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "0.0.1"
 
-provides 'custom'
-provides 'custom::install_sqs_river_plugin'
+#provides 'custom'
+#provides 'custom::install_sqs_river_plugin'
--- custom/recipes/default.rb
+++ custom/recipes/default.rb
@@ -1,5 +1,5 @@
-pacakge "custom"
-
-include_recipe "custom::#{node['custom']['install_method']}"
+package "install_sqs_river_plugin" do
+  command "/usr/local/elasticsearch/bin/plugin -i river-amazonsqs -u https://s3-eu-west-1.amazonaws.com/albogdano/river-amazonsqs.zip"
+end
 
 #include_recipe "custom::install_sqs_river_plugin"
